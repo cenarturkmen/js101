5981b5190aed10a5558793b275762b63872a7807
caner <canerturkmen@msn.com>
2021-07-07 00:55:43 +0300

    local storage added but some features are missing

--- todo-app/index.js
+++ todo-app/index.js
@@ -5,25 +5,18 @@ const main = document.getElementById("main");
 const clearAll = document.getElementById("todo-clearAllButton");
 const clearFinished = document.getElementById("todo-clearDoneButton");
 const par = document.getElementById("paragraphs");
+const localStorageButton = document.getElementById("todo-checkLocalStorage");
 
+addButton.addEventListener("click", function(e,todo1){
+    const todo = input.value;
+    const todoType = "notDone";
 
-addButton.addEventListener("click", function(){
-    let paragraph = document.createElement("p");
-
-    paragraph.innerText = input.value;
-    par.appendChild(paragraph);
-    input.value = "";
-    paragraph.addEventListener("click", function(){
-        if (paragraph.style.textDecoration === "line-through"){
-            paragraph.style["textDecoration"] = "none";
-            paragraph.style["opacity"] =1;
-            
-        }
-        else{
-            paragraph.style["textDecoration"] = "line-through";
-            paragraph.style["opacity"] =0.5;
-        }
-    })
+    //for local storage
+    if (todo && todoType){
+        localStorage.setItem(todo, todoType);
+        //location.reload();
+    }
+   addParagraph();    
 });
 
 
@@ -53,3 +46,38 @@ input.addEventListener("keyup", function(e){
         addButton.click();
     }
 })
+
+//local storage reverter
+localStorageButton.addEventListener("click", function(e){
+    
+    for(let i=0; i<localStorage.length; i++){
+        const todo = localStorage.key(i);
+        console.log(todo);
+        addParagraph(todo);
+
+    }
+})
+
+function addParagraph(todoLS){
+    let paragraph = document.createElement("p");
+    if (todoLS){
+        paragraph.innerText = todoLS;
+        console.log(todoLS);
+    }
+    else{
+        paragraph.innerText = input.value;
+    }
+    par.appendChild(paragraph);
+    paragraph.addEventListener("click", function(){
+        if (paragraph.style.textDecoration === "line-through"){
+            paragraph.style["textDecoration"] = "none";
+            paragraph.style["opacity"] =1;
+            
+        }
+        else{
+            paragraph.style["textDecoration"] = "line-through";
+            paragraph.style["opacity"] =0.5;
+        }
+    })
+
+}
