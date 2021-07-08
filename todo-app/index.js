87f37e7aad203453edc0c44c88e139827043464f
caner <canerturkmen@msn.com>
2021-07-08 17:41:34 +0300

    local storage added

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,4 +1,4 @@
-//
+// variables
 const input = document.getElementById("todo-input");
 const addButton = document.getElementById("todo-addButton");
 const main = document.getElementById("main");
@@ -7,6 +7,7 @@ const clearFinished = document.getElementById("todo-clearDoneButton");
 const par = document.getElementById("paragraphs");
 const localStorageButton = document.getElementById("todo-checkLocalStorage");
 
+// add todo
 addButton.addEventListener("click", function(e,todo1){
     const todo = input.value;
     const todoType = "notDone";
@@ -14,12 +15,11 @@ addButton.addEventListener("click", function(e,todo1){
     //for local storage
     if (todo && todoType){
         localStorage.setItem(todo, todoType);
-        //location.reload();
     }
-   addParagraph();    
+   addParagraph();   
 });
 
-
+// clear done tasks
 clearFinished.addEventListener("click", function(){
     let allParagraphs = document.querySelectorAll("p");
 
@@ -30,16 +30,17 @@ clearFinished.addEventListener("click", function(){
     }
 });
 
-
+// clear all tasks also local storage
 clearAll.addEventListener("click", function(){
     let allParagraphs = document.querySelectorAll("p");
 
     for(i=0; i<allParagraphs.length; i++){
         allParagraphs[i].innerHTML = "";
     }
+    localStorage.clear();
 });
 
-
+// if you press enter then this function trigger addButton
 input.addEventListener("keyup", function(e){
 
     if (e.key == "Enter"){
@@ -52,32 +53,45 @@ localStorageButton.addEventListener("click", function(e){
     
     for(let i=0; i<localStorage.length; i++){
         const todo = localStorage.key(i);
-        console.log(todo);
-        addParagraph(todo);
-
+        if (localStorage.getItem(todo) == "notDone"){
+            addParagraph(todo);
+        }
+        else{
+            addParagraph(todo, 2);
+        }
     }
 })
 
-function addParagraph(todoLS){
+// addButton and localStorageButton use this. 
+function addParagraph(todoLS, notDone){
     let paragraph = document.createElement("p");
     if (todoLS){
         paragraph.innerText = todoLS;
-        console.log(todoLS);
+
+        if (notDone){
+            paragraph.style["textDecoration"] = "line-through";
+            paragraph.style["opacity"] =0.5;
+        }
     }
     else{
         paragraph.innerText = input.value;
+        input.value = "";
     }
+
     par.appendChild(paragraph);
+    
     paragraph.addEventListener("click", function(){
         if (paragraph.style.textDecoration === "line-through"){
             paragraph.style["textDecoration"] = "none";
-            paragraph.style["opacity"] =1;
-            
+            paragraph.style["opacity"] =0.90;
+            localStorage.setItem(paragraph.innerText, "notDone");
+            console.log(paragraph.innerText, localStorage.getItem(paragraph.innerText)); 
         }
         else{
             paragraph.style["textDecoration"] = "line-through";
             paragraph.style["opacity"] =0.5;
+            localStorage.setItem(paragraph.innerText, "Done");
+            console.log(paragraph.innerText, localStorage.getItem(paragraph.innerText));
         }
     })
-
 }
