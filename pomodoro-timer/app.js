26d32de8a1133d368586e692f1f0bd1e4b34a639
caner <canerturkmen@msn.com>
2021-07-26 11:30:14 +0300

    app added

---
+++ pomodoro-timer/app.js
@@ -0,0 +1,33 @@
+var minute = 29;
+var second = 59;
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const endButton = document.querySelector("#end")
+var clock;
+
+startButton.addEventListener("click", function(){
+    clock = setInterval(function(){
+        document.querySelector("#timer").innerHTML = minute + " : " + second;
+        second--;
+        if (second==0 ){
+            minute--;
+            second = 60;
+            if (minute == 0){
+                alert("Time is up");
+                minute = 30;
+            }
+        }
+    },500);
+});
+
+pauseButton.addEventListener("click", function(){
+    clearInterval(clock);
+    
+})
+
+endButton.addEventListener("click", function(){
+    clearTimeout(clock);
+    second= 60;
+    minute = 29;
+    document.querySelector("#timer").innerHTML = minute + " : " + second;
+})
